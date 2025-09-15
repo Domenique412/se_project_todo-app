be13374493ed4bea930790e6ec11ffce7fbe54a7
Domenique412 <dwatkinswise@gmail.com>
2025-09-15 10:51:33 -0400

    Popup added

---
+++ components/Popup.js
@@ -0,0 +1,21 @@
+class Popup {
+    constructor(popupSelector)
+
+    open() {
+
+    }
+
+    close() {
+
+    }
+
+    _handleEscapeClose() {
+
+    }
+
+    setEventListners() {
+
+    }
+}
+
+export default Popup;
